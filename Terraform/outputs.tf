bf81e4f528a38e0f8d1479d78270a35b9083280f
Prayankag <131856399+Prayankag@users.noreply.github.com>
2024-12-24 05:28:02 +0000

    Update outputs.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,19 +1,24 @@
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
+output "ecs_cluster_name" {
+  description = "The ECS cluster name"
+  value       = module.ecs.cluster_name
 }
 
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane"
-  value       = module.eks.cluster_security_group_id
+output "ecs_service_arns" {
+  description = "ARNs of the ECS services"
+  value       = module.ecs.service_arns
+}
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "subnets" {
+  description = "List of subnets"
+  value       = module.vpc.public_subnets
 }
 
 output "region" {
   description = "AWS region"
   value       = var.region
-}
-
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = module.eks.cluster_name
 }
